e00080faeaf603d6ed5c3dd29030a60221e9dc36
froissak <kevin.froissart69@gmail.com>
2020-03-05 09:27:15 +0100

    List devient tableau

--- src/main/java/fr/ulille/iut/pizzaland/dto/PizzaCreateDto.java
+++ src/main/java/fr/ulille/iut/pizzaland/dto/PizzaCreateDto.java
@@ -1,12 +1,10 @@
 package fr.ulille.iut.pizzaland.dto;
 
-import java.util.List;
-
 import fr.ulille.iut.pizzaland.beans.Ingredient;
 
 public class PizzaCreateDto {
 	private String name;
-	private List<Ingredient> listeIngredient;
+	private Ingredient[] listeIngredient;
 
 	public PizzaCreateDto() {}
 
@@ -18,11 +16,11 @@ public class PizzaCreateDto {
 		return name;
 	}
 	
-	public List<Ingredient> getListeIngredient() {
+	public Ingredient[] getListeIngredient() {
 		return listeIngredient;
 	}
 
-	public void setListeIngredient(List<Ingredient> listeIngredient) {
+	public void setListeIngredient(Ingredient[] listeIngredient) {
 		this.listeIngredient = listeIngredient;
 	}
 }
--- src/main/java/fr/ulille/iut/pizzaland/dto/PizzaDto.java
+++ src/main/java/fr/ulille/iut/pizzaland/dto/PizzaDto.java
@@ -7,7 +7,7 @@ import fr.ulille.iut.pizzaland.beans.Ingredient;
 public class PizzaDto {
 	private long id;
 	private String name;
-	private List<Ingredient> listeIngredient;
+	private Ingredient[] listeIngredient;
 
 	public PizzaDto() {}
 
@@ -27,11 +27,11 @@ public class PizzaDto {
 		return name;
 	}
 
-	public List<Ingredient> getListeIngredient() {
+	public Ingredient[] getListeIngredient() {
 		return listeIngredient;
 	}
 
-	public void setListeIngredient(List<Ingredient> listeIngredient) {
+	public void setListeIngredient(Ingredient[] listeIngredient) {
 		this.listeIngredient = listeIngredient;
 	}
 }
